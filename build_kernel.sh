a8603db0a1929af205083b22fded9d71f4deca60
LGVsus <lg.vsus@gmail.com>
2026-05-21 18:54:15 -0500

    test3

--- build_kernel.sh
+++ build_kernel.sh
@@ -3,11 +3,5 @@
 export ARCH=arm64
 export PLATFORM_VERSION=13
 
-make ARCH=arm64 mrproper
-
 make ARCH=arm64 physwizz_defconfig
-
-make ARCH=arm64 \
-     KCFLAGS="--no-cuda-version-check" \
-     CLANG_FLAGS="--no-cuda-version-check" \
-     -j$(nproc)
+make ARCH=arm64 -j16
